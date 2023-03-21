4672225ac6132f21dfbed3f790f230dc54d709f7
Billie Zhang <billie.zhang1504@gmail.com>
2023-03-20 21:28:23 -0400

    upload files

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import About from './About';
+import Button from './Button';
+import Footer from './Footer';
+import Hero from './Hero';
+import Prototype from './Prototype';
+import Navbar from  './Navbar';
+
+export {
+	About,
+	Button, 
+	Footer,
+	Hero,
+	Prototype,
+	Navbar,
+}
